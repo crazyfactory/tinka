7dac9d449b03f80066fe00cb60617c36f8c3b09d
Nishchal <n.gautam@crazy-factory.com>
2016-10-04 16:49:56 +0700

    Fixes #3 along with other minor changes

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ gulp.task('compile-w', function(){
 });
 
 gulp.task('tslint', function() {
-  return gulp.src('app/bootstrap.ts')
+  return gulp.src('app/httpClient.ts')
     .pipe(tslint())
     .pipe(tslint.report('verbose'));
 });
