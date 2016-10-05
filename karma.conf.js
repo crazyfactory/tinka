366bc4a35e713953826d44c5a6db72c9c39b2612
Nishchal <n.gautam@crazy-factory.com>
2016-10-05 13:15:40 +0700

    test

--- karma.conf.js
+++ karma.conf.js
@@ -66,8 +66,13 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
+    browsers: ['Chrome_travis_ci'],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
