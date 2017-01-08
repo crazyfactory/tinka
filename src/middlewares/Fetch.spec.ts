e2c1c4a221cd601237005757cfbe2821fea7ebd6
Nishchal Gautam <gautam.nishchal@gmail.com>
2017-01-08 17:01:43 +0700

    test(Fetch): Wrap similar tests in a `describe` block

--- src/middlewares/Fetch.spec.ts
+++ src/middlewares/Fetch.spec.ts
@@ -2,20 +2,27 @@ import {IRequest, IRequestHeaders} from "../Client";
 import {Fetch} from "./Fetch";
 
 describe("Fetch", () => {
+    describe("is defined and is available", () => {
+        it("should be defined", () => {
+            const fetch = new Fetch();
+            expect(fetch).toBeDefined();
+        });
+    });
     describe("process()", () => {
         it("is defined", () => {
             const obj = new Fetch();
             expect(obj.process).toBeDefined();
         });
 
-        it("throws on non-object options", () => {
-            const obj = new Fetch();
-            expect(() => obj.process(false as any, null as any)).toThrow();
-        });
-
-        it("throws on null options", () => {
-            const obj = new Fetch();
-            expect(() => obj.process(null as any, null as any)).toThrow();
+        describe("Should throw on invalid options", () => {
+            it("throws on non-object options", () => {
+                const obj = new Fetch();
+                expect(() => obj.process(false as any, null as any)).toThrow();
+            });
+            it("throws on null options", () => {
+                const obj = new Fetch();
+                expect(() => obj.process(null as any, null as any)).toThrow();
+            });
         });
 
         it("returns a promise", () => {
