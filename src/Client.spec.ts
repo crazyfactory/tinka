f7b1f44df753ca2604b5a31b10b2dcf4875b9946
wmathes <fanatik@gmx.at>
2017-01-13 14:18:02 +0700

    test(Client): Simplified

--- src/Client.spec.ts
+++ src/Client.spec.ts
@@ -10,47 +10,21 @@ describe("Client", () => {
         });
         it("accepts as defaultOptions-object", () => {
             const options: IRequest = {
-                baseUrl: "http://api.example.com",
-                method: "POST",
-                queryParameters: {
-                    user: "1",
-                    id: "2"
-                },
-                headers: {
-                    "Accept": "",
-                    "Authorization": "",
-                    "Content-Type": "application/json",
-                    "Accept-Language": "en"
-                }
+                method: "POST"
             };
 
             const client = new Client(options);
 
-            expect(client.defaultOptions.baseUrl).toBe("http://api.example.com");
-            expect(client.defaultOptions.headers["Accept-Language"]).toBe("en");
             expect(client.defaultOptions.method).toBe("POST");
         });
         it("accepts a defaultOptions-function", () => {
             const fn = () => {
                 return {
-                    baseUrl: "http://api.example.com",
                     method: "POST",
-                    queryParameters: {
-                        user: "1",
-                        id: "2"
-                    },
-                    headers: {
-                        "Accept": "",
-                        "Authorization": "",
-                        "Content-Type": "application/json",
-                        "Accept-Language": "en"
-                    }
                 } as IRequest;
             };
             const client = new Client(fn);
 
-            expect(client.defaultOptions.baseUrl).toBe("http://api.example.com");
-            expect(client.defaultOptions.headers["Accept-Language"]).toBe("en");
             expect(client.defaultOptions.method).toBe("POST");
         });
     });
