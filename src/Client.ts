845f82f6018e019dbe69afcc27430238d4c2ee18
paibamboo <gavinsitti@hotmail.com>
2017-09-26 08:58:17 +0700

    refactor(typings): add type def to Client

--- src/Client.ts
+++ src/Client.ts
@@ -1,10 +1,10 @@
 /**
  * @module tinka
  */
-import {FetchMiddleware, IFetchRequest} from "./middlewares/FetchMiddleware";
+import {FetchMiddleware, IFetchRequest, IFetchResponse} from "./middlewares/FetchMiddleware";
 import {Stack} from "./Stack";
 
-export class Client extends Stack<any, any> {
+export class Client extends Stack<IFetchRequest, Promise<IFetchResponse<any>>> {
     constructor(defaultOptions?: IFetchRequest) {
         super();
         this.addMiddleware(new FetchMiddleware(defaultOptions));
